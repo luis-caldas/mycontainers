7e27b8a49480f97c24bdf8650833cd864cec062f
Luis <admin@luiscaldas.com>
2021-03-06 21:31:24 +0000

    removed ip caching due to unstable ip acquisition

--- build/update-dns/update_dns.sh
+++ build/update-dns/update_dns.sh
@@ -4,12 +4,7 @@
 echo "$KEY"
 
 # Cache files
-IP_FILE="/var/cache/dns_updater/ipaddress"
 KEY_FILE="/var/cache/dns_updater/alphanumeric"
-
-# Extract the current IP
-CURRENT_IP="$(/usr/bin/host myip.opendns.com | grep "has address" | cut -d ' ' -f 4)"
-REGISTERED_IP="Unknown"
 
 # Check if the key is cached or not
 if [ -e "$KEY_FILE" ]; then
@@ -21,21 +16,8 @@ else
 	exit 1
 fi
 
-# Check if the IP is cached
-if [ -e "$IP_FILE" ]; then
-	REGISTERED_IP="$(cat "$IP_FILE")"
-fi
+# Update the DNS entry
+echo "Updating IP address ..."
+curl -s -k "http://freedns.afraid.org/dynamic/update.php?""${KEY}"
 
-# Compare to see if the IPs changed
-# If yes, update the DNS entry
-if [ ! "$CURRENT_IP" = "$REGISTERED_IP" ]; then
-
-	# Update it
-	echo "Updating IP address from $REGISTERED_IP to $CURRENT_IP ..."
-	curl -s -k "http://freedns.afraid.org/dynamic/update.php?""${KEY}"
-
-	# Update the cache file with the IP as well
-	echo "$CURRENT_IP" > "$IP_FILE"
-
-fi
  
